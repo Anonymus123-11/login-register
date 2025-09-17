153738c57f4e99fb249bcbe1c8c981a9f5964161
ino004 <haovhse182170@fpt.edu.vn>
2025-09-17 11:06:52 +0700

    update

--- swagger.js
+++ swagger.js
@@ -1,3 +1,5 @@
+// config/swagger.js
+require("dotenv").config();
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 
@@ -10,26 +12,22 @@ function setupSwagger(app) {
         version: "1.0.0",
         description: "API for user authentication with JWT",
       },
-      servers: [
-        {
-          url: "http://localhost:5000",
-          description: "Local server"
-        },
-        {
-          url: "https://login-register-dtsi.onrender.com",
-          description: "Production server (Render)"
-        }
-      ],
       components: {
         securitySchemes: {
           bearerAuth: { type: "http", scheme: "bearer", bearerFormat: "JWT" },
         },
       },
+      servers: [
+        { url: process.env.LOCAL_PUBLIC_BASE_URL, description: "Local server" },
+        { url: process.env.RENDER_PUBLIC_BASE_URL, description: "Render server" },
+      ],
     },
     apis: ["./routes/*.js"],
   };
 
   const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+  // Không cần middleware để set servers nữa
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 }
 
